aa233a25e1f6d5300fce7daab343336b6d326a1a
ShriyaBade <119793647+ShriyaBade@users.noreply.github.com>
2023-11-07 13:19:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {Routes, Route} from 'react-router-dom'
+import {Index} from './Components/Index'
+import {AllJobs} from './Components/AllJobs'
+import { AboutUs } from './Components/AboutUs'
+import {Contact} from './Components/Contact'
+import {LogIn} from './Components/LogIn'
+import {SignUp} from './Components/SignUp'
+import { Edit } from './Components/Edit'
+import { Navbar } from './Components/Navbar'
+import { NewDash } from './Components/NewDash'
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Index/>}></Route>
+      <Route path='/dashboard' element={<NewDash/>}></Route>
+      <Route path='/alljobs' element={<AllJobs/>}></Route>
+      <Route path='/about' element={<AboutUs/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
+      <Route path='/login' element={<LogIn/>}></Route>
+
+      <Route path='/signup' element={<SignUp/>}></Route>
+      <Route path='/edit' element={<Edit/>}></Route>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
